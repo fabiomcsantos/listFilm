558e033625673bbfe46974091806108bb96eb55e
fabiomcsantos <binhomenini@gmail.com>
2024-08-19 20:12:31 -0300

    my first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+
+import { React, useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+import "./MovieGrid.css"
+
+const movieURL = import.meta.VITE_API;
+const apiKey = import.meta.VITE_API_KEY;
+
+const Home= () => {
+    const [topMovies, setTopMovies] = useState([]);
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch (url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
+    };
+
+    useEffect (() => {
+        const getTopRatedUrl = `${movieURL}top_rated?${apiKey}`;
+        
+        getTopRatedMovies(getTopRatedUrl);
+
+    }, []);
+
+    return(
+        <div className="container">
+            <h2 className="title">Melhores Filmes:</h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>Carregando...</p>}
+                {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
